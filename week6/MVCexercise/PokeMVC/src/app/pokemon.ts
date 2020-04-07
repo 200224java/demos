e6eaeaa0de71dcc8db479e2b03e1feffefbe6e04
gattietime <gattietime@gmail.com>
2020-04-07 13:17:29 -0400

    Removed unnecessary files

--- week6/MVCexercise/PokeMVC/src/app/pokemon.ts
+++
@@ -1,9 +0,0 @@
-import { Trainer } from './trainer';
-
-export class Pokemon {
-    public pokeId:number;
-    public pokedexId:number;
-    public name:String;
-    public baseXP:number;
-    public trainer:Trainer
-}
--- week6/MVCexercise/PokeMVC/src/app/trainer.ts
+++
@@ -1,7 +0,0 @@
-import { Pokemon } from './pokemon';
-
-export class Trainer {
-    public tId:number;
-    public name:String;
-    public party:Array<Pokemon>
-}
